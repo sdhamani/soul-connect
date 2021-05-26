db95bc644563eb883ea279d674b35bcd998a5909
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-05-27 02:05:14 +0530

    Added a new idea in idea's array

--- src/data/ideas.js
+++ src/data/ideas.js
@@ -1,12 +1,23 @@
 let ideas = [
   {
-    id: "i124",
+    id: "i001",
     userId: "u123",
-    title: "",
-    description: "",
+    title: "Company Portal",
+    description:
+      "To create a company portal which can have information about the company's establishment.It should include the company's achievements. A photo gallery with photos of all events of the company. It can also have the information about the company's future events. Also, the top performer from each unit can be displayed on the portal",
     tags: ["feature", "tech"],
     creationDate: "5/27/21",
     votes: 12,
+  },
+  {
+    id: "i002",
+    userId: "u124",
+    title: "Messaging Application",
+    description:
+      "Creating an internal messaging app for respective teams where team can share the sprint planning and tasks. The application can be team specific.",
+    tags: ["tech"],
+    creationDate: "5/29/21",
+    votes: 7,
   },
 ];
 
