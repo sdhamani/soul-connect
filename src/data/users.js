3966070e45dd66651c119168b2ba1257c4443a36
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-05-27 01:46:46 +0530

    Created users static array

---
+++ src/data/users.js
@@ -0,0 +1,11 @@
+let users = [
+  {
+    id: "u123",
+    name: "Sagar Dhamani",
+    employeeId: "SB153560",
+    password: "Sagar@123",
+    ideasPosted: ["i124"],
+  },
+];
+
+export default users;
