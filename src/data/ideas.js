f46760e013cbe5eaf26edd6517cdad4a626448b4
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-05-27 01:46:58 +0530

    Created static ideas array

---
+++ src/data/ideas.js
@@ -0,0 +1,13 @@
+let ideas = [
+  {
+    id: "i124",
+    userId: "u123",
+    title: "",
+    description: "",
+    tags: ["feature", "tech"],
+    creationDate: "5/27/21",
+    votes: 12,
+  },
+];
+
+export default ideas;
