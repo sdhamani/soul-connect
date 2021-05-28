9421ecb4db28659097f88b01b36cb6e2949f4ffd
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-05-29 02:45:01 +0530

    Created Sidebar component

---
+++ src/components/sidebar/SideBar.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import "./sidebar.css";
+import useLogin from "../../context/login-context";
+import users from "../../data/users";
+
+function SideBar() {
+  const { userName, employeeId } = useLogin();
+  console.log(userName, employeeId);
+  const user = users.find((user) => user.employeeId === employeeId);
+  return (
+    <div className="sidebar">
+      <div>
+        <div className="sidebar-heading">
+          <img
+            className="sidebar-userImage"
+            alt="userImage"
+            src={user?.profileImage}
+          ></img>
+          <span className="sidebar-userName">{userName}</span>
+        </div>
+
+        <div className="sidebade-myposts">My Posts</div>
+      </div>
+    </div>
+  );
+}
+
+export default SideBar;
