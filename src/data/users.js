94f1dfd5275dfcaa0aea1d4873d6c8fa122a6203
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-05-28 19:01:15 +0530

    Added profile imgae to users array

--- src/data/users.js
+++ src/data/users.js
@@ -4,6 +4,7 @@ let users = [
     name: "Sagar Dhamani",
     employeeId: "SB153560",
     password: "Sagar@123",
+    profileImage: "https://serene-ui.netlify.app/Images/profile-photo.jpg",
     ideasPosted: ["i001"],
   },
   {
@@ -11,6 +12,8 @@ let users = [
     name: "Anurag Rathod",
     employeeId: "SB133562",
     password: "Anurag@123",
+    profileImage:
+      "https://media-exp1.licdn.com/dms/image/C5603AQFFH3w21RpPWg/profile-displayphoto-shrink_200_200/0/1613718546410?e=1627516800&v=beta&t=U55M40amCg7LWadPjn5vKnnQFNfgxTIek9MP-qiToHI",
     ideasPosted: ["i124"],
   },
 ];
