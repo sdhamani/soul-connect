3314fcae858578e53df1b2feeacb8f40a4bf0e59
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-06-01 00:48:25 +0530

    Added reducer funtionality to manage ideas

--- src/context/ideas-context.js
+++ src/context/ideas-context.js
@@ -1,7 +1,8 @@
 import { createContext, useContext } from "react";
-import { useState } from "react";
+import { useState, useReducer } from "react";
 import allIdeas from "../data/ideas";
-
+import users from "../data/users";
+import useLogin from "./login-context";
 const IdeasContainer = createContext();
 
 export default function useIdeas() {
@@ -10,13 +11,60 @@ export default function useIdeas() {
 
 export function IdeasProvider({ children }) {
   const initialIdeas = allIdeas;
-  const [ideas, setIdeas] = useState(initialIdeas);
+  const { employeeId } = useLogin();
+  const loggedInUser = users.find((user) => user.employeeId === employeeId);
+
+  const [ideas, ideasDispatch] = useReducer(dispatchfun, initialIdeas);
+
+  function dispatchfun(state, value) {
+    console.log({ state }, { value });
+    const ideaId = value.PAYLOAD;
+    switch (value.TYPE) {
+      case "VOTES":
+        return state.slice().sort(function (a, b) {
+          let aVotesLength = a.votes.length;
+          let bVotesLength = b.votes.length;
+          return bVotesLength - aVotesLength;
+        });
+
+      case "EARLIEST_DATE":
+        return state.slice().sort(function (a, b) {
+          a = a.creationDate.split("/");
+          b = b.creationDate.split("/");
+          return a[2] - b[2] || a[1] - b[1] || a[0] - b[0];
+        });
+      case "OLDEST_DATE":
+        return state.slice().sort(function (a, b) {
+          a = a.creationDate.split("/");
+          b = b.creationDate.split("/");
+          return b[2] - a[2] || b[1] - a[1] || b[0] - a[0];
+        });
+
+      case "ADDIDEA":
+        return [...state, value.PAYLOAD];
+      case "LIKE":
+        return state.map((idea) => {
+          if (idea.id === ideaId) {
+            return idea.votes.includes(loggedInUser.id)
+              ? {
+                  ...idea,
+                  votes: idea.votes.find((vote) => vote !== loggedInUser.id),
+                }
+              : { ...idea, votes: [...idea.votes, loggedInUser.id] };
+          }
+          return idea;
+        });
+
+      default:
+        return state;
+    }
+  }
 
   return (
     <IdeasContainer.Provider
       value={{
         ideas: ideas,
-        setIdeas: setIdeas,
+        ideasDispatch: ideasDispatch,
       }}
     >
       {children}
--- src/data/ideas.js
+++ src/data/ideas.js
@@ -7,8 +7,8 @@ let allIdeas = [
     description:
       "To create a company portal which can have information about the company's establishment.It should include the company's achievements. A photo gallery with photos of all events of the company. It can also have the information about the company's future events. Also, the top performer from each unit can be displayed on the portal",
     tags: ["feature", "tech"],
-    creationDate: "27/5/21",
-    votes: ["u123"],
+    creationDate: "27/5/2021",
+    votes: ["u124"],
   },
   {
     id: "i002",
@@ -18,7 +18,7 @@ let allIdeas = [
     description:
       "Creating an internal messaging app for respective teams where team can share the sprint planning and tasks. The application can be team specific.",
     tags: ["tech"],
-    creationDate: "29/5/21",
+    creationDate: "29/5/2021",
     votes: ["u123", "u124"],
   },
 ];
