175ace9d1111b7829cd9f23f096590e5c051b9a5
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-06-01 03:05:08 +0530

    Added userposts route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Login from "./components/login/Login";
 import { Routes, Route } from "react-router-dom";
 import PrivateRoute from "./routes/PrivateRoute";
 import HomePage from "./components/homepage/HomePage";
+import UserPosts from "./components/userposts/UserPosts";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <PrivateRoute path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
+        <PrivateRoute path="/usersposts" element={<UserPosts />} />
       </Routes>
     </div>
   );
