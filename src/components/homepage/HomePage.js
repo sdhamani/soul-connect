6443a5d5fdb9b19440c376a79a80b956af6f8675
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-06-01 14:03:22 +0530

    Added homepage deadline

--- src/components/homepage/HomePage.js
+++ src/components/homepage/HomePage.js
@@ -10,6 +10,12 @@ export default function HomePage() {
   return (
     <div className="homepage-div">
       <Nav />
+
+      <span className="homepage-deadline">
+        The Ideas for June hackathon will be accepted till date 20th June 2021.
+        Please make sure you submit your ideas before the deadline.
+      </span>
+
       <div className="homepage-content">
         <SideBar />
         <div className="homepage-posts">
