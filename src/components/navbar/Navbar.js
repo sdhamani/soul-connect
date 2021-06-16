df47bc3823b84b3a00d42e918be8d1a89bdde339
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-06-17 03:46:24 +0530

    Added firebase logout functionality

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import "./navbar.css";
 import { Link } from "react-router-dom";
 import useLogin from "../../context/login-context";
+import firebase from "firebase";
 
 import SideBar from "../sidebar/SideBar";
 
@@ -10,16 +11,28 @@ export default function Nav({ route, setRoute }) {
   const navigate = useNavigate();
   const { loggedIn, setloggedIn, userName } = useLogin();
   const [showSide, setshowSide] = useState(false);
+  var firebaseConfig = {
+    apiKey: "AIzaSyArlrzqz4RbnazxWwOk6AGKoebkjU1TyqA",
+    authDomain: "social-media-b028f.firebaseapp.com",
+    projectId: "social-media-b028f",
+    storageBucket: "social-media-b028f.appspot.com",
+    messagingSenderId: "551745934551",
+    appId: "1:551745934551:web:e603b9d485a94b5f000c92",
+    measurementId: "G-SNMB1BE0P6",
+  };
 
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  } else {
+    firebase.app();
+  }
   function logoutFun() {
+    firebase.auth().signOut();
     setloggedIn(false);
-
-    localStorage?.setItem("login", JSON.stringify({ isUserLoggedIn: false }));
     localStorage?.setItem(
-      "localUserName",
-      JSON.stringify({ localUserName: "" })
+      "user",
+      JSON.stringify({ isUserLoggedIn: false, localUserName: "" })
     );
-    localStorage?.setItem("employeeId", JSON.stringify({ employeeId: "" }));
 
     navigate("/login");
   }
@@ -42,6 +55,14 @@ export default function Nav({ route, setRoute }) {
             Hack Ideas
           </Link>
         </h1>
+        <form class="search-user">
+          <input
+            class="search-user-input"
+            type="search"
+            placeholder="Search user"
+            aria-label="Search"
+          />
+        </form>
         {loggedIn === true ? (
           <div className="logout-div">
             <h3 className="nav-userName">Hi {userName} !</h3>
