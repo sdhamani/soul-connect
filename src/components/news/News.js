843a229466e715836b47c795e1e1c4be8cc3ac81
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-06-17 03:45:41 +0530

    Updated news

--- src/components/news/News.js
+++ src/components/news/News.js
@@ -14,7 +14,6 @@ function News() {
           Deadline to submit projects for this month extended till 15th June.
         </li>
         <li>Damage assesment app wins the April Hackathon </li>
-
         <li>
           Project with changes in chat bot brings increased customer
           satisfaction
@@ -23,9 +22,7 @@ function News() {
           Several ideas about healthcare system have been selected for the
           second stage.
         </li>
-        <li>
-          "Its great to see so many great ideas come up" - CTO of Scripbox
-        </li>
+        <li>"Its great to see so many great ideas come up" - CTO</li>
         <li>April's Hackthon records the highest number of entries ever.</li>
       </ul>
     </div>
