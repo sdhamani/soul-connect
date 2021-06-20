b137d1891d7e0fdc8426f7537b7dcaa48f0ba841
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-06-20 14:06:50 +0530

    Updated static users array

--- src/data/users.js
+++ src/data/users.js
@@ -5,6 +5,8 @@ let users = [
     employeeId: "SB153560",
     password: "Sagar@123",
     profileImage: "https://serene-ui.netlify.app/Images/profile-photo.jpg",
+    following: ["u124"],
+    followers: ["u124"],
     ideasPosted: ["i001"],
   },
   {
@@ -12,6 +14,8 @@ let users = [
     name: "Anurag Rathod",
     employeeId: "SB133562",
     password: "Anurag@123",
+    following: ["U9tuKCrtktd1VZyagZhoes5EN0g1"],
+    followers: ["U9tuKCrtktd1VZyagZhoes5EN0g1"],
     profileImage:
       "https://media-exp1.licdn.com/dms/image/C5603AQFFH3w21RpPWg/profile-displayphoto-shrink_200_200/0/1613718546410?e=1627516800&v=beta&t=U55M40amCg7LWadPjn5vKnnQFNfgxTIek9MP-qiToHI",
     ideasPosted: ["i124"],
